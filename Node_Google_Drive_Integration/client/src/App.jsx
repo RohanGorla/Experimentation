933dd321e28445f4852e819490f35a459ae14223
RohanGorla <rohangorla2010@gmail.com>
2024-10-16 23:01:10 +0530

    successfully set up client server model to get all images in drive and show them on the client web page

--- Node_Google_Drive_Integration/client/src/App.jsx
+++ Node_Google_Drive_Integration/client/src/App.jsx
@@ -1,13 +1,25 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [files, setFiles] = useState("");
+  const [images, setImages] = useState([]);
 
   async function handleSubmit(e) {
     e.preventDefault();
     console.log(files);
   }
+
+  async function getPhotos() {
+    const response = await axios.get("http://localhost:8008/getphotos");
+    console.log(response.data);
+    setImages(response.data);
+  }
+
+  useEffect(() => {
+    getPhotos();
+  }, []);
 
   return (
     <>
@@ -21,6 +33,10 @@ function App() {
         ></input>
         <input type="submit"></input>
       </form>
+      {images.map((image, index) => {
+        let url = "https://drive.google.com/thumbnail?id=" + image.id;
+        return <img key={index} src={url}></img>;
+      })}
     </>
   );
 }
--- Node_Google_Drive_Integration/server/server.js
+++ Node_Google_Drive_Integration/server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { google } from "googleapis";
 import fs from "fs";
+import { url } from "inspector";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 8008;
 const SCOPE = ["https://www.googleapis.com/auth/drive"];
@@ -25,6 +28,26 @@ async function uploadFiles() {
   const auth = await authorizeUser();
   const drive = google.drive({ version: "v3", auth: auth });
 
+  //   const getResponse = await drive.files.list({});
+  //   console.log("Get response -> ", getResponse.data);
+
+  //   getResponse.data.files.map(async (file) => {
+  //     if (file.mimeType.split("/")[0] == "image") {
+  //       await drive.files.delete({
+  //         fileId: file.id,
+  //       });
+  //     }
+  //   });
+
+  //   const response = await drive.files.list({});
+  //   if (response.data.files.length) {
+  //     console.log(response.data.files.length);
+  //     response.data.files.map(async (file) => {
+  //       console.log(file);
+  //       const response = await drive.files.delete({});
+  //     });
+  //   }
+
   const fileMetaData = {
     name: "test_image.jpeg",
     parents: [process.env.ID],
@@ -39,14 +62,42 @@ async function uploadFiles() {
     media: media,
     fields: "id",
   });
-  console.log(response.data.id);
   return response.data.id;
 }
 
-uploadFiles();
+// uploadFiles();
 
 app.get("/", (req, res) => {
   res.json("Listening...");
+});
+
+app.get("/getphotos", async (req, res) => {
+  const auth = await authorizeUser();
+  const drive = google.drive({ version: "v3", auth: auth });
+  const getResponse = await drive.files.list({});
+  let urls = [];
+  for (let i = 0; i < getResponse.data.files.length; i++) {
+    if (getResponse.data.files[i].mimeType.split("/")[0] == "image") {
+      drive.permissions.create({
+        fileId: getResponse.data.files[i].id,
+        requestBody: {
+          role: "reader",
+          type: "anyone",
+        },
+      });
+      const result = await drive.files.get({
+        fileId: getResponse.data.files[i].id,
+        fields: "webViewLink, webContentLink",
+      });
+      let data = {
+        view: result.data.webViewLink,
+        download: result.data.webContentLink,
+        id: getResponse.data.files[i].id,
+      };
+      urls.push(data);
+    }
+  }
+  res.send(urls);
 });
 
 app.listen(PORT, () => {
