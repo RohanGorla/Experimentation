797e74fff084801f71f06ae739b29279e90b538f
RohanGorla <rohangorla2010@gmail.com>
2024-11-30 18:43:06 +0530

    Add socket connection and send html file from server

--- Web_Socket_Basic_Setup/server/server.js
+++ Web_Socket_Basic_Setup/server/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import { createServer } from "node:http";
+import { Server } from "socket.io";
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 import "dotenv/config";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const PORT = process.env.PORT || 8008;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Web Sockets</h1>");
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 server.listen(PORT, () => {
