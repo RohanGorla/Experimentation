d8792690b47e1c59d58ae530cc96f02c8a8d0fc3
RohanGorla <rohangorla2010@gmail.com>
2024-12-02 03:03:49 +0530

    Add a get route to get all the records data in the mongo db collection

--- Node_MongoDB_Integration/server.js
+++ Node_MongoDB_Integration/server.js
@@ -4,10 +4,22 @@ import { MongoClient } from "mongodb";
 
 const app = express();
 
-const client = new MongoClient("mongodb://localhost:27017");
+const PORT = process.env.PORT;
+
+const client = new MongoClient(process.env.DB_URL);
 client
   .connect()
   .then(() => {
     console.log("MongoDB Connected!");
   })
   .catch((err) => console.log(err));
+
+const db = client.db(process.env.DB_NAME);
+const collection = db.collection("test1");
+
+app.get("/", async (req, res) => {
+  const result = await collection.find().toArray();
+  res.json(result);
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
