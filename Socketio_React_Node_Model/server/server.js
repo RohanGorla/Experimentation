9e0995e090a2801fdf881055e8f924839965d59a
RohanGorla <rohangorla2010@gmail.com>
2024-12-01 16:07:27 +0530

    Made basic socket server with cors in server side and made connection

--- Socketio_React_Node_Model/server/server.js
+++ Socketio_React_Node_Model/server/server.js
@@ -5,13 +5,17 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server({
+const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json("connected");
+});
 
 io.on("connection", (socket) => {
   console.log("A connection has been made!");
