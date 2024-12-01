297fac1189f28adc2ae460c60ffd785e57dd138c
RohanGorla <rohangorla2010@gmail.com>
2024-12-01 16:48:51 +0530

    create a basic message form in client side

--- Socketio_React_Node_Model/client/src/App.jsx
+++ Socketio_React_Node_Model/client/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:5000");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+
+  async function submitMessage(e) {
+    e.preventDefault();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={submitMessage}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        ></input>
+        <input type="submit" name="Send"></input>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
